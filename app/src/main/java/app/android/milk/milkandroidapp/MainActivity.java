7e6a9759aea9e5b8ed7353bbedbe4dad1be4ab7a
Billy Lai <billyinthe510@yahoo.com>
2018-03-20 23:56:27 -0700

    Created a CameraPreview Class extending SurfaceView

--- app/src/main/java/app/android/milk/milkandroidapp/MainActivity.java
+++ app/src/main/java/app/android/milk/milkandroidapp/MainActivity.java
@@ -264,6 +264,17 @@ public class MainActivity extends AppCompatActivity
         return hasCamera;
     }
     //endregion
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
     //region Dialog Box Sample
     public void ShowDialog()
